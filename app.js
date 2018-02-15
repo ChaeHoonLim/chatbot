edcd2a37042ad0af61ed7f41d89f90786f0f0718
ChaeHoonLim <lch.hoon1986@gmail.com>
2018-02-15 23:19:38 +0900

    STT

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ require('dotenv').config('./.env');
 var restify     = require('restify');
 var builder     = require('./core/');
 const log4js    = require('log4js');
-var utils       = require('./utils/util');
 var logger      = log4js.getLogger('worker');
 var speechService = require('./speech-service.js');
 
@@ -561,3 +560,64 @@ bot.dialog('schedule', function (session, args) {
 matches: 'schedule'
 });
 
+
+
+
+
+
+
+
+
+/*
+    Utils
+*/
+function hasAudioAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        (session.message.attachments[0].contentType === 'audio/wav' ||
+            session.message.attachments[0].contentType === 'application/octet-stream');
+}
+
+function getAudioStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+function processText(text) {
+    var result = 'You said: ' + text + '.';
+
+    if (text && text.length > 0) {
+        var wordCount = text.split(' ').filter(function (x) { return x; }).length;
+        result += '\n\nWord Count: ' + wordCount;
+
+        var characterCount = text.replace(/ /g, '').length;
+        result += '\n\nCharacter Count: ' + characterCount;
+
+        var spaceCount = text.split(' ').length - 1;
+        result += '\n\nSpace Count: ' + spaceCount;
+
+        var m = text.match(/[aeiou]/gi);
+        var vowelCount = m === null ? 0 : m.length;
+        result += '\n\nVowel Count: ' + vowelCount;
+    }
+
+    return result;
+}
